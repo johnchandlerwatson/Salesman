64801b751c34f2c4f47abc59f4f0703726c2d3a0
chandler <johnchandlerwatson@gmail.com>
2018-08-12 13:06:45 -0700

    basic app running

---
+++ ClientApp/components/app-root.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="app" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12">
+                <router-view></router-view>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import HomePage from './home-page'
+
+Vue.component('home-page', HomePage);
+
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ ClientApp/components/home-page.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Hello weary salesman!</h1>
+        <p>I will help guide you along your journey</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<style>
+</style>
